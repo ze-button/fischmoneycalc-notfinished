737bf2bb0fecdc6073303c13240a131c4f38981a
ze-button <zebutton69@gmail.com>
2026-03-03 16:30:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import sympy as sp
 
 st.set_page_config(page_title="Fisch Calc", page_icon="🐟")
 
@@ -39,6 +40,35 @@ with col2:
         m_ch = c[0].number_input(f"Mut {i+1} %", value=0.0, step=0.01, format="%.2f", key=f"mch{i}")
         m_val = c[1].number_input(f"Mut {i+1} Mult", value=0.0, key=f"mval{i}")
         m_data.append((m_ch, m_val))
+        
+#mathforruinous
+if passive_spec == "Onirifalx":
+    if st.button("RUN CALCULATOR", type="primary"):
+        spark_m = (spark_ch * 0.85)/100 + 1
+        shiny_m = (shin_ch * 0.85)/100 + 1
+        lure_speed = max(0,1-(lure_spd/100))
+    
+        avg_f_val = sum(f[0] * f[1] for f in f_data)/100
+        avg_f_speed = sum(f[0] * f[2] for f in f_data)
+        avg_mut_m = sum(m[0] * m[1] for m in m_data)/100
+    
+        val_mult = avg_mut_m * size_mult * shiny_m * spark_m
+        total_speed = rod_speed + avg_f_speed
+        
+        x = sp.symbols('x') #sympy stuff
+        f_x = 6.8 / (1 + (total_speed + 5 * x) / 100)
+        solutions = sp.solve(f_x - x, x)
+        positive_solutions = [float(sol) for sol in solutions if sol > 0]
+        
+        time_to_catch = positive_solutions + 1.2 + 1 + lure_speed
+        catches = time_given / time_to_catch
+        total_money = (avg_f_val * val_mult) * catches
+        st.divider()
+        st.metric("Total Money", f"{total_money:,.0f} C$")
+        st.write(f"**Total Catches:** {catches:.1f}")
+        st.write(f"**Catch Speed:** {time_to_catch:.2f}s")
+
+
 # mathforonirifalx
 if passive_spec == "Onirifalx":
     if st.button("RUN CALCULATOR", type="primary"):
