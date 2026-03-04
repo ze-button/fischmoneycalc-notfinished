98aee12b620e7f1a1317f6b66ffe58df208dd9db
zebutton <zebutton69@gmail.com>
2026-03-04 08:32:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ with st.sidebar:
     lure_spd = st.number_input("Lure Speed", value=0)
     passive_spec = st.selectbox(
     "Rod Passive (WIP)",
-    options=["None", "Dead Man's Rod", "Ruinous", "Onirifalx"])
+    options=["None", "Dead Man's Rod", "Ruinous", "Onirifalx","Luminescent"])
     if passive_spec == "None":
         rod_name = st.text_input("Rod Name")
 
@@ -42,6 +42,32 @@ with col2:
         m_ch = c[0].number_input(f"Mut {i+1} %", value=0.0, step=0.01, format="%.2f", key=f"mch{i}")
         m_val = c[1].number_input(f"Mut {i+1} Mult", value=0.0, key=f"mval{i}")
         m_data.append((m_ch, m_val))
+
+
+#mathforluminescent
+if passive_spec == "Luminescent":
+    if st.button("RUN CALCULATOR", type="primary"):
+        spark_m = (spark_ch * 0.85)/100 + 1
+        shiny_m = (shin_ch * 0.85)/100 + 1
+        lure_speed = max(0,1-(lure_spd/100))
+    
+        avg_f_val = sum(f[0] * f[1] for f in f_data)/100
+        avg_f_speed = sum(f[0] * f[2] for f in f_data)/100
+        avg_mut_m = sum(m[0] * m[1] for m in m_data)/100
+    
+        val_mult = avg_mut_m * size_mult * shiny_m * spark_m
+        total_speed = rod_speed + avg_f_speed
+    
+        time_to_catch = 6.8 / ((total_speed / 100) + 1 ) * ((0.15*(55/80)+(0.85))) + 1.2 + 1 + lure_speed
+        catches = time_given / time_to_catch
+        total_money = (avg_f_val * val_mult) * catches
+        avg_fish_val = avg_f_val*val_mult
+        st.divider()
+        st.metric("Total Money made with Dead Man's Rod", f"{total_money:,.0f} C$")
+        st.write(f"**Total Catches:** {catches:.1f}")
+        st.write(f"**Catch Speed:** {time_to_catch:.2f}s")
+        st.write(f"**Average Fish Value:** {avg_fish_val:.2f}")
+        
 
 #mathsfordeadmans
 if passive_spec == "Dead Man's Rod":
