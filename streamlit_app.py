f39f70e78462efdfaec6e1f076d356ec94e1710b
zebutton <zebutton69@gmail.com>
2026-03-05 21:25:21 +0100

    Update streamlit_app.py
    added wind ele

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ with st.sidebar:
     mutt_count = st.number_input("Mutation Count", value=3)
     passive_spec = st.selectbox(
     "Rod Passive (WIP)",
-    options=["None", "Dead Man's Rod", "Ruinous", "Onirifalx","Luminescent","Seraphic"])
+    options=["None", "Dead Man's Rod", "Ruinous", "Onirifalx","Luminescent","Seraphic","Wind Elemental"])
     if passive_spec == "None":
         rod_name = st.text_input("Rod Name")
 
@@ -47,6 +47,31 @@ with col2:
         m_val = c[1].number_input(f"Mut {i+1} Mult", value=0.0, key=f"mval{i}")
         m_data.append((m_ch, m_val))
 
+
+#mathsforwindele
+if passive_spec == "Wind Elemental":
+    if st.button("RUN CALCULATOR", type="primary"):
+        spark_m = (spark_ch * 0.85)/100 + 1
+        shiny_m = (shin_ch * 0.85)/100 + 1
+        lure_speed = max(0,1-(lure_spd/100))
+    
+        avg_f_val = sum(f[0] * f[1] for f in f_data)/100
+        avg_f_speed = sum(f[0] * f[2] for f in f_data)/100
+        avg_mut_m = sum(m[0] * m[1] for m in m_data)/100
+    
+        val_mult = avg_mut_m * size_mult * shiny_m * spark_m
+        total_speed = rod_speed + avg_f_speed
+    
+        time_to_catch = 6.8 / ((total_speed / 100) + 1 ) * (50/85) + 1.2 + 1 + lure_speed
+        catches = time_given / time_to_catch
+        total_money = (avg_f_val * val_mult) * catches
+        avg_fish_val = avg_f_val*val_mult
+        st.divider()
+        st.metric("Total Money made with Dead Man's Rod", f"{total_money:,.0f} C$")
+        st.write(f"**Total Catches:** {catches:.1f}")
+        st.write(f"**Catch Speed:** {time_to_catch:.2f}s")
+        st.write(f"**Average Fish Value:** {avg_fish_val:.2f}")
+
 #mathsforseraphic
 if passive_spec == "Seraphic":
     if st.button("RUN CALCULATOR", type="primary"):
@@ -61,7 +86,7 @@ if passive_spec == "Seraphic":
         val_mult = avg_mut_m * size_mult * shiny_m * spark_m
         total_speed = rod_speed + avg_f_speed
     
-        time_to_catch = 6.8 / ((total_speed / 100) + 1 ) * (35/80) + 1.2 + 1 + lure_speed
+        time_to_catch = 6.8 / ((total_speed / 100) + 1 ) * (40/85) + 1.2 + 1 + lure_speed
         catches = time_given / time_to_catch
         total_money = (avg_f_val * val_mult) * catches
         avg_fish_val = avg_f_val*val_mult
