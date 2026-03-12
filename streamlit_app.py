d0f87d5c3491d2acb9c287b78f5b28f7cda17f1a
zebutton <zebutton69@gmail.com>
2026-03-12 20:27:43 +0100

    Update streamlit_app.py
    fixed inconsistencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,13 @@ with st.sidebar:
     st.header("Specifications")
     time_given = st.number_input("Time (seconds)", value=750)
     rod_speed = st.number_input("Rod Prog Speed", value=0)
-    size_mult = st.number_input("Size Multiplier", value=1.0)
-    spark_ch = st.number_input("Sparkling Chance %", value=1,min_value=1, max_value=100)
-    shin_ch = st.number_input("Shiny Chance %", value=1,min_value=1, max_value=100)
+    size_multiplier = st.number_input("Size Multiplier", value=1.0)
+    sparkling_chance = st.number_input("Sparkling Chance %", value=1,min_value=1, max_value=100)
+    shiny_chance = st.number_input("Shiny Chance %", value=1,min_value=1, max_value=100)
     lure_spd = st.number_input("Lure Speed", value=0)
-    fsh_count = st.number_input("Fish Count", value=3)
-    mutt_count = st.number_input("Mutation Count", value=3)
-    passive_spec = st.selectbox(
+    fish_count = st.number_input("Fish Count", value=3)
+    mutation_count = st.number_input("Mutation Count", value=3)
+    passive_specification = st.selectbox(
     "Rod Passive (WIP)",
     options=["None", "Dead Man's Rod", "Ruinous", "Onirifalx","Luminescent","Seraphic","Wind Elemental"])
     rod_name = st.text_input("Rod Name")
@@ -30,26 +30,26 @@ col1, col2 = st.columns(2)
 with col1:
     st.subheader("Fish Stats")
     st.caption("Make sure the chances add up to 100%")
-    f_data = []
-    for i in range(fsh_count):
-        c = st.columns(3)
-        ch = c[0].number_input(f"Fish {i+1} %", value=0.0, step=0.01, format="%.2f", key=f"fch{i}")
-        val = c[1].number_input(f"Fish {i+1} C$", value=0.0, key=f"fval{i}")
-        spd = c[2].number_input(f"Fish {i+1} PrgSpd", value=0.0, key=f"fspd{i}")
-        f_data.append((ch, val, spd))
+    fish_data = []
+    for i in range(fish_count):
+        column = st.columns(3)
+        fih_chance = column[0].number_input(f"Fish {i+1} %", value=0.0, step=0.01, format="%.2f", key=f"fch{i}")
+        fih_avg_value = column[1].number_input(f"Fish {i+1} C$", value=0.0, key=f"fval{i}")
+        fih_progress_speed = column[2].number_input(f"Fish {i+1} PrgSpd", value=0.0, key=f"fspd{i}")
+        fish_data.append((fih_chance, fih_avg_value, fih_progress_speed))
 
 with col2:
     st.subheader("Mutations")
     st.caption("Make sure the chances add up to 100%")
-    m_data = []
-    for i in range(mutt_count):
-        c = st.columns(2)
-        m_ch = c[0].number_input(f"Mut {i+1} %", value=0.0, step=0.01, format="%.2f", key=f"mch{i}")
-        m_val = c[1].number_input(f"Mut {i+1} Mult", value=0.0, key=f"mval{i}")
-        m_data.append((m_ch, m_val))
+    mutation_data = []
+    for i in range(mutation_count):
+        column = st.columns(2)
+        mut_chance = column[0].number_input(f"Mut {i+1} %", value=0.0, step=0.01, format="%.2f", key=f"mch{i}")
+        mut_value = column[1].number_input(f"Mut {i+1} Mult", value=0.0, key=f"mval{i}")
+        mutation_data.append((mut_chance, mut_value))
 
 
-# mathfornone
+# math
 row1_col1, row1_col2, row1_col3 = st.columns([1, 1, 1])
 with row1_col2:
     st.write(" ")
@@ -63,73 +63,73 @@ with row2_col2:
 
 
 if run_calc:
-        if passive_spec == "None":
+        if passive_specification == "None":
             specific_name = rod_name
         else:
-            specific_name = passive_spec
+            specific_name = passive_specification
         
-        spark_m = (spark_ch * 0.85)/100 + 1
-        shiny_m = (shin_ch * 0.85)/100 + 1
+        sparkling_chance_final = (sparkling_chance * 0.85)/100 + 1
+        shiny_chance_final = (shiny_chance * 0.85)/100 + 1
         lure_speed = max(0,1-(lure_spd/100))
     
-        avg_f_val = sum(f[0] * f[1] for f in f_data)/100
-        avg_f_speed = sum(f[0] * f[2] for f in f_data)/100
-        avg_mut_m = sum(m[0] * m[1] for m in m_data)/100
-        total_speed = rod_speed + avg_f_speed
+        average_fish_value = sum(f[0] * f[1] for f in fish_data)/100
+        average_fish_prog_speed = sum(f[0] * f[2] for f in fish_data)/100
+        average_mutation_multiplier = sum(m[0] * m[1] for m in mutation_data)/100
+        total_lure_speed = rod_speed + average_fish_prog_speed
 
         #sympy stuff
-        x = sp.symbols('x') 
-        f_x = 6.8 / (1 + (total_speed + 5 * x) / 100)
-        solutions = sp.solve(f_x - x, x)
-        positive_solutions = [float(sol) for sol in solutions if sol > 0]
+        x = sp.symbols('x') #define x
+        f_x = 6.8 / (1 + (total_lure_speed + 5 * x) / 100) #define f(x)
+        solutions = sp.solve(f_x - x, x) #find f(x)=x
+        positive_solutions = [float(sol) for sol in solutions if sol > 0] #use the positive solution
 
         #passivemulti
-        if passive_spec == "Ruinous":
-            passives_multi=((0.15*(20/85))+(0.85))
-        elif passive_spec == "Wind Elemental":
-            passives_multi=(50/85)
-        elif passive_spec == "Luminescent":
-            passives_multi=((0.15*(60/85)+(0.85)))
-        elif passive_spec == "Seraphic":
-            passives_multi=(40/85)
-        elif passive_spec=="Onirifalx":
-            passives_multi=((50/85*0.3)+0.7)
-        elif passive_spec=="Dead Man's Rod":
-            passives_multi=(45/85)
-        elif passive_spec=="None":
-            passives_multi=1
+        if passive_specification == "Ruinous":
+            passives_exponent=((0.15*(20/85))+(0.85))
+        elif passive_specification == "Wind Elemental":
+            passives_exponent=(50/85)
+        elif passive_specification == "Luminescent":
+            passives_exponent=((0.15*(60/85)+(0.85)))
+        elif passive_specification == "Seraphic":
+            passives_exponent=(40/85)
+        elif passive_specification=="Onirifalx":
+            passives_exponent=((50/85*0.3)+0.7)
+        elif passive_specification=="Dead Man's Rod":
+            passives_exponent=(45/85)
+        elif passive_specification=="None":
+            passives_exponent=1
 
         #timetocatchformula
-        if passive_spec == "Ruinous":
-            time_to_catch_formul= positive_solutions[0]
+        if passive_specification == "Ruinous":
+            time_to_catch_formula= positive_solutions[0]
         else:
-            time_to_catch_formul= (6.8 / ((total_speed / 100) + 1))
+            time_to_catch_formula= (6.8 / ((total_lure_speed / 100) + 1))
 
         #finalstuff
-        val_mult = avg_mut_m * size_mult * shiny_m * spark_m
-        time_to_catch = (time_to_catch_formul*passives_multi)+ 1.2 + 1 + lure_speed
+        value_multiplier = average_mutation_multiplier * size_multiplier * shiny_chance_final * sparkling_chance_final
+        time_to_catch = (time_to_catch_formula*passives_exponent)+ 1.2 + 1 + lure_speed
         catches = time_given / time_to_catch
-        total_money = (avg_f_val * val_mult) * catches
-        avg_fish_val = avg_f_val*val_mult
+        total_money_made = (average_fish_value * value_multiplier) * catches
+        average_fish_final_value = average_fish_value * value_multiplier
         st.divider()
-        st.metric(f"Total Money made with {specific_name}:" , f"{total_money:,.0f} C$")
-        if passive_spec != "None":
+        st.metric(f"Total Money made with {specific_name}:" , f"{total_money_made:,.0f} C$")
+        if passive_specification != "None":
             st.write(f"({rod_name})")
         st.write(f"**Total Catches:** {catches:.1f}")
         st.write(f"**Catch Speed:** {time_to_catch:.2f}s")
-        st.write(f"**Average Fish Value:** {avg_fish_val:.2f}")
+        st.write(f"**Average Fish Value:** {average_fish_final_value:.2f}")
 
                 # export
         # results
-        if passive_spec == "None":
+        if passive_specification == "None":
             export_data = {
-                "A": ["Rod","TotalMoney", "TotalCatches", "TimeGiven", "Time-to-catch", "AvgFishVal", "AvgFishValMultip"],
-                "B": [specific_name, total_money, catches, time_given, time_to_catch, avg_fish_val, val_mult]
+                "Column 1": ["Rod","TotalMoney", "TotalCatches", "TimeGiven", "Time-to-catch", "AvgFishVal", "AvgFishValMultip"],
+                "Column 2": [specific_name, total_money_made, catches, time_given, time_to_catch, average_fish_final_value, value_multiplier]
             }
         else:
                         export_data = {
-                "A": ["Rod","TotalMoney", "TotalCatches", "TimeGiven", "Time-to-catch", "AvgFishVal", "AvgFishValMultip", "Spec"],
-                "B": [specific_name, total_money, catches, time_given, time_to_catch, avg_fish_val, val_mult, rod_name]
+                "Column 1": ["Rod","TotalMoney", "TotalCatches", "TimeGiven", "Time-to-catch", "AvgFishVal", "AvgFishValMultip", "Spec"],
+                "Column 2": [specific_name, total_money_made, catches, time_given, time_to_catch, average_fish_final_value, value_multiplier, rod_name]
             }
         df = pd.DataFrame(export_data)
 
